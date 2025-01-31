7eaacf69e6a63ec3349a7b9104094e864f96c978
Pradeep Khatri <pradeepkhatri44@live.com>
2025-01-31 22:53:33 +0530

    Complete Random Color Generator
    Complete Random Color Generator

---
+++ App.js
@@ -0,0 +1,10 @@
+import randomColor from "./src/components/random-color";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Random Color Component */}
+      <randomColor />
+    </div>
+  );
+}
